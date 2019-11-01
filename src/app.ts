825478205c196bf743745971aaaef577ce5f9985
Luka Brinar <luka.brinar@dlabs.si>
2019-11-01 16:46:23 +0100

    Static-properties-methods

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,8 @@
-interface SizesInterface {
-    sizes: string[];
-    availableSizes: string[];
-}
-
-abstract class Sizes implements SizesInterface {
-    constructor(public sizes: string[]) {}
-
-    set availableSizes(sizes: string[]) {
-        this.sizes = sizes;
-    }
-
-    get availableSizes () {
-        return this.sizes;
+class Coupon {
+    static allowed = ['Pepperoni', 'Blazing inferno'];
+    static create(percentage: number) {
+        return `PIZZA_RESTAURANT_${percentage}`;
     }
 }
 
-interface PizzaInterface extends SizesInterface {
-    readonly name: string;
-    toppings: string[];
-    updateSizes(sizes: string[]): void;
-    addTopping(topping: string): void;
-}
-
-class Pizza extends Sizes implements PizzaInterface {
-    public toppings: string[] = [];
-
-    constructor(readonly name: string, sizes: string[]) {
-        super(sizes);
-    }
-
-    public addTopping (topping: string) {
-        this.toppings.push(topping);
-    }
-
-    public updateSizes (sizes: string[]) {
-        this.sizes = sizes;
-    }
-
-}
-
-const pizza = new Pizza('Pepperoni', ['small', 'medium']);
-
-console.log(pizza.availableSizes);
-
-pizza.updateSizes(['large']);
-
-console.log(pizza.availableSizes);
+console.log(Coupon.create(25));
