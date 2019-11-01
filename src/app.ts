912a603325659a5c0893f352e637bd7e0ea76896
Luka Brinar <luka.brinar@dlabs.si>
2019-11-01 17:18:24 +0100

    Reverse an array or string

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,17 @@
-class Pizza {
-    constructor(private name: string, private price: number){
+function reverse (str: string): string;
+function reverse<T> (arr: T[]): T[];
+
+
+function reverse<T> (stringOrArray: string | T[]): string | T[] {
+    if (typeof stringOrArray === 'string') {
+        return stringOrArray
+        .split('')
+        .reverse()
+        .join('');
     }
+    return stringOrArray.slice().reverse();
 }
 
-class List<T> {
-    private list: T[];
 
-    addItem(item: T): void {
-        this.list.push(item);
-    }
-
-    getList(): T[] {
-        return this.list;
-    }
-
-}
-
-const list = new List<Pizza>();
-
-list.addItem(new Pizza('Pepperoni', 15));
-
-const pizzas = list.getList();
-
-class Coupon {
-    constructor(private name: string) {
-
-    }
-}
-
-const anotherList = new List<Coupon>();
-anotherList.addItem(new Coupon('Pizza25'));
+reverse('Pepperoni');
+reverse(['bacon','pepperoni', 'chilli', 'mushrooms']);
